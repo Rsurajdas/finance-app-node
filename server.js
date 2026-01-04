2942cff79b188f538f43f0bb24f6d232bcc4acf6
Suraj Kumar <suraj.k@avantaifintech.com>
2026-01-04 11:15:46 +0530

    configure server.js file and connected mongodb database

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-import app from "./app";
+/* eslint-disable no-console */
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+import app from "./app.js";
+
+dotenv.config({ path: "./.env", override: true });
+
+async function main() {
+  // eslint-disable-next-line no-undef
+  await mongoose.connect(process.env.MONGODB_URI);
+}
+
+main()
+  .then(() => console.log("DB connected successfully..."))
+  .catch((err) => console.log(err));
+
+// eslint-disable-next-line no-undef
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App listening to ${port}`);
+});
