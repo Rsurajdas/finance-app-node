62aac671af68a33996bc078aeb56f02ddab16d39
Suraj Kumar <suraj.k@avantaifintech.com>
2026-01-04 10:21:52 +0530

    configure app.js file and access log stream file

--- app.js
+++ app.js
@@ -4,10 +4,25 @@ import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
+import rfs from "rotating-file-stream";
 import { fileURLToPath } from "url";
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url); // Get the current file path
 const __dirname = path.dirname(__filename); // Get the current directory path
+
+app.use("/assets", express.static(path.join(__dirname, "public", "assets"))); // Serve static assets from the "public/assets" directory
+app.use(express.json()); // Parse JSON request bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded request bodies
+app.use(compression()); // Enable response compression
+app.use(cors()); // Enable CORS
+app.use(helmet()); // Set security-related HTTP headers
+
+const accessLogStream = rfs.createStream("access.log", {
+  interval: "1d", // rotate log files daily
+  size: "10M", // rotate every 10 MegaBytes written
+});
+
+app.use(morgan("combined", { stream: accessLogStream }));
 
 export default app;
