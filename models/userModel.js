9f97e35817e38be3199643bae1ab2d50366bf086
Suraj Kumar <suraj.k@avantaifintech.com>
2026-01-09 22:10:50 +0530

    created user model Pre save middleware and correctPassword & changePasswordAfter method

--- models/userModel.js
+++ models/userModel.js
@@ -1,36 +1,38 @@
-import { Schema, model } from "mongoose";
-import validator from "validator";
+import bcrypt from 'bcryptjs';
+import { Schema, model } from 'mongoose';
+import validator from 'validator';
 
 const userSchema = new Schema({
-  fullname: {
+  fullName: {
     type: String,
-    require: [true, "Please enter your username."],
-    minLength: [3, "Username must be at least 3 characters long."],
+    require: [true, 'Please enter your username.'],
+    minLength: [3, 'Username must be at least 3 characters long.'],
   },
   email: {
     type: String,
-    require: [true, "Please enter your email."],
-    validate: [validator.isEmail, "Please provide a valid email address."],
+    require: [true, 'Please enter your email.'],
+    validate: [validator.isEmail, 'Please provide a valid email address.'],
     unique: true,
     lowercase: true,
   },
   password: {
     type: String,
-    require: [true, "Please enter your password."],
-    minLength: [8, "Password must be at least 8 characters long."],
+    require: [true, 'Please enter your password.'],
+    minLength: [8, 'Password must be at least 8 characters long.'],
     validate: [
       validator.isStrongPassword,
-      "Password must contain at least 8 characters, including uppercase, lowercase, number, and symbol",
+      'Password must contain at least 8 characters, including uppercase, lowercase, number, and symbol',
     ],
     select: false,
   },
   confirmPassword: {
     type: String,
+    require: [true, 'Please confirm your password'],
     validate: {
       validator: function (el) {
         return el === this.password;
       },
-      message: "Passwords do not match",
+      message: 'Passwords do not match',
     },
     select: false,
   },
@@ -39,11 +41,36 @@ const userSchema = new Schema({
     default: true,
     select: false,
   },
-  passwordChnageAt: Date,
+  passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetTokenExpires: Date,
 });
 
-const User = model("Users", userSchema);
+userSchema.pre('save', async function () {
+  // Only hash password, when password is modified or created new
+  if (!this.isModified('password')) return;
+  this.password = await bcrypt.hash(this.password, 12);
+  this.confirmPassword = undefined;
+});
+
+userSchema.pre('save', function () {
+  if (!this.isModified('password') || this.isNew) return;
+  this.passwordChangedAt = Date.now() - 1000;
+});
+
+userSchema.methods.correctPassword = async function (receivedPassword, storedPassword) {
+  return await bcrypt.compare(this.password, storedPassword);
+};
+
+userSchema.methods.changePasswordAfter = function (jwtTimestamp) {
+  if (this.passwordChangedAt) {
+    const changeTimeStamp = parseInt(this.passwordChangeAt.getTime() / 1000, 10);
+    return jwtTimestamp < changeTimeStamp;
+  }
+  // False means password not changed
+  return false;
+};
+
+const User = model('Users', userSchema);
 
 export default User;
