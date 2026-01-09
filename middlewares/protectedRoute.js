8aaeb0e6c90d3dfbeb97946936a74f75165ea368
Suraj Kumar <suraj.k@avantaifintech.com>
2026-01-09 22:09:40 +0530

    created protected Route middleware

---
+++ middlewares/protectedRoute.js
@@ -0,0 +1,37 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'node:util';
+
+import User from '../models/userModel.js';
+import AppError from '../utils/appError.js';
+import { catchAsync } from '../utils/catchAsync.js';
+
+const protectedRoute = catchAsync(async (req, res, next) => {
+  let token;
+
+  if (req.headers.authorization || req.headers.authorization.startsWith('Bearer ')) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(new AppError('You are not logged in! Please log in to get access', 401));
+  }
+
+  // eslint-disable-next-line no-undef
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  const user = await User.findById(decoded.id);
+
+  if (!user) {
+    return next(new AppError('The user belonging to this token does no longer exist', 401));
+  }
+
+  if (user.changePasswordAfter(decoded.iat)) {
+    return next(new AppError('User recently changed password, Please login again!'));
+  }
+
+  req.user = user;
+
+  next();
+});
+
+export default protectedRoute;
