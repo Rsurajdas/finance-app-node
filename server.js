718ecc1a4072ebd687ac6724483dbbfdc555c379
Suraj Kumar <suraj.k@avantaifintech.com>
2026-01-10 13:08:10 +0530

    installed and configure redis

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-console */
-import dotenv from "dotenv";
-import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import { createClient } from 'redis';
 
-import app from "./app.js";
+import app from './app.js';
 
-dotenv.config({ path: "./.env", override: true });
+dotenv.config({ path: './.env', override: true });
 
 async function main() {
   // eslint-disable-next-line no-undef
@@ -12,8 +13,23 @@ async function main() {
 }
 
 main()
-  .then(() => console.log("DB connected successfully..."))
+  .then(() => console.log('DB connected successfully...'))
   .catch((err) => console.log(err));
+
+const client = createClient({
+  // eslint-disable-next-line no-undef
+  username: process.env.REDIS_USERNAME,
+  // eslint-disable-next-line no-undef
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: 'redis-10354.crce217.ap-south-1-1.ec2.cloud.redislabs.com',
+    port: 10354,
+  },
+});
+
+client.on('error', (err) => console.log('Redis Client Error', err));
+
+await client.connect();
 
 // eslint-disable-next-line no-undef
 const port = process.env.PORT || 3000;
