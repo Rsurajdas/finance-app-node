c4f77342f33ad09582d41cfbb432c8caa84ab996
Suraj Kumar <suraj.k@avantaifintech.com>
2026-01-11 13:09:16 +0530

    category schema created

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,17 +1,35 @@
 import { Schema, model } from 'mongoose';
+import slugify from 'slugify';
 
 const categorySchema = new Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, 'Category name is required'],
     minlength: [3, 'Name should be at least 3 characters'],
+    unique: true,
     trim: true,
   },
   slug: {
     type: String,
-    required: true,
     unique: true,
+    index: true,
   },
+  description: {
+    type: String,
+    trim: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    index: true,
+  },
+});
+
+categorySchema.pre('save', function () {
+  if (this.isModified('name')) {
+    this.slug = slugify(this.name, { lower: true });
+  }
 });
 
 const Category = model('Category', categorySchema);
